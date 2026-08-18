d63b11ec46ee430b7a08ffd2d844711aa49a6be1
kuinone <kuinone@qq.com>
2026-08-18 10:04:36 +0800

    Feat: 各类盔甲初步测试

--- src/main/java/com/equilibrium/Config.java
+++ src/main/java/com/equilibrium/Config.java
@@ -6,7 +6,7 @@ import net.minecraft.resources.ResourceLocation;
 
 public class Config {
 
-    public static final boolean IS_DEBUG = true;
+    public static final boolean IS_DEBUG = false;
 
     private static boolean validateItemName(final Object obj) {
         return obj instanceof String itemName && BuiltInRegistries.ITEM.containsKey(ResourceLocation.parse(itemName));
